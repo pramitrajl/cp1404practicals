9a63b386653070525b0847f66b10caaa633bc56f
pramitrajl <pramitraj.luintel@my.jcu.edu.au>
2025-05-28 17:38:03 +1000

    Refactor password check program to use functions

---
+++ prac_02/score_menu.py
@@ -0,0 +1,49 @@
+menu = """ Menu:
+(G)et a valid score
+(P)rint result
+(S)how stars
+(Q)uit"""
+
+def main():
+    score = 0
+    print(menu)
+    choice = input("Enter your choice: ").upper()
+    while choice!= 'Q':
+        if choice == 'G':
+            score = get_valid_score("Enter your score: ", 0, 100)
+        elif choice == 'P':
+            grade = get_score_result(score)
+            print(grade)
+
+        elif choice == 'S':
+            show_stars(score)
+        else:
+            print("Invalid Choice")
+        choice = input("Enter your choice: ").upper()
+    print("Farewell")
+
+def get_valid_score(prompt, low, high):
+    marks = int(input(prompt))
+    while marks < low or marks > high:
+        print("Invalid score")
+        marks = int(input(prompt))
+    return marks
+
+def show_stars(score):
+    for i in range (score):
+        print("*", end='')
+        print()
+
+def get_score_result(score):
+    """ Determine result """
+    if score < 0 or score > 100:
+        return "Invalid score"
+    else:
+        if score >= 90:
+            return "Excellent"
+        elif score >= 50:
+            return "Pass"
+        else:
+            return "Bad"
+main()
+    
