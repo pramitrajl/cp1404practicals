17ba6b946bb5c4b8201eb5f62a94d94526498afc
pramitrajl <pramitraj.luintel@my.jcu.edu.au>
2025-06-03 17:23:17 +1000

    Show use of random function

---
+++ prac_03/randoms.py
@@ -0,0 +1,14 @@
+# What did you see on line 1? What was the smallest number you could have seen, what was the largest?
+# --- I saw a single integer value i.e "7". Smallest number could be 5 and largest could be 20.
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+#--- I saw integer value "7" in line 2. Smallest number could be 3 and largest could be 9. No.
+
+#What did you see on line 3?
+#What was the smallest number you could have seen, what was the largest?
+#--- I saw 5.09005833048135 in line 3. Smallest could be 2.5 and largest could be 5.5
+
+import random
+print(random.randint(1,100))
