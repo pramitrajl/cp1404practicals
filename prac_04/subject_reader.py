c6e5dcab57840c35d579bf3bc9b6c54cccb24ded
pramitrajl <pramitraj.luintel@my.jcu.edu.au>
2025-06-11 18:44:39 +1000

    add lists program

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -12,6 +12,7 @@ def main():
 
 
 def load_data():
+    data=[]
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
     for line in input_file:
@@ -21,9 +22,13 @@ def load_data():
         parts = line.split(',')  # Separate the data into its parts
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
+        print(parts)
+        print(f"{parts[0]} is taught by {parts[1]} and has {parts[2]} students")# See if that worked
         print("----------")
+        data.append(parts)
     input_file.close()
+    return data
+
 
 
 main()
